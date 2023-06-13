3e1474f5d4abf6ee23c119135164241a11c8acce
Chen Wen Kang <23054115+cwkang1998@users.noreply.github.com>
2023-06-14 01:22:20 +0800

    fix: ci does not error with missing locale (#34)
    CI Throw Error on non existing keys

--- main.js
+++ main.js
@@ -1,53 +1,65 @@
-const fs = require("fs")
+const fs = require("fs");
 
-const ignoreFiles = [".github", ".git", "Contribution.md", "LICENSE", "main.js", "README.md"]
+const ignoreFiles = [
+  ".github",
+  ".git",
+  "Contribution.md",
+  "LICENSE",
+  "main.js",
+  "README.md",
+];
 
 function getFiles(dir, files = []) {
-    // Get an array of all files and directories in the passed directory using fs.readdirSync
-    const fileList = fs.readdirSync(dir);
+  // Get an array of all files and directories in the passed directory using fs.readdirSync
+  const fileList = fs.readdirSync(dir);
 
-    const filteredFileList = fileList.filter(file => !ignoreFiles.includes(file))
+  const filteredFileList = fileList.filter(
+    (file) => !ignoreFiles.includes(file)
+  );
 
-    for (const file of filteredFileList) {
+  for (const file of filteredFileList) {
+    const name = `${dir}/${file}`;
 
-        const name = `${dir}/${file}`;
-
-        if (fs.statSync(name).isDirectory()) {
-            getFiles(name, files);
-        } else {
-            files.push(name);
-        }
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files);
+    } else {
+      files.push(name);
     }
-    return files;
+  }
+  return files;
 }
 
 const main = () => {
-    // We already know this
-    const enPath = "./en/translation.json";
-    const enData = fs.readFileSync(enPath, {
-        encoding: "utf-8"
-    });
-    const targetKeys = Object.keys(JSON.parse(enData));
+  // We already know this
+  const enPath = "./en/translation.json";
+  const enData = fs.readFileSync(enPath, {
+    encoding: "utf-8",
+  });
+  const targetKeys = Object.keys(JSON.parse(enData));
 
-    const files = getFiles(".");    
+  const files = getFiles(".");
 
-    files.forEach((file) => {
-        if (file != enPath) {
-            try {
-                const data = fs.readFileSync(file, {
-                    encoding: "utf-8"
-                });
-                const thisLang = JSON.parse(data);
-                const thisLangKeys = Object.keys(thisLang);
-                let not_included = targetKeys.filter((t_key) => !thisLangKeys.includes(t_key));
-                console.warn(file)
-                console.warn("This Locales doesn't have following Keys:")
-                console.warn(not_included)
-            } catch (e) {
-                console.error(e);
-            }
-        }
-    })
-}
+  files.forEach((file) => {
+    if (file != enPath) {
+        console.info(`Checking locale ${file}`)
+      let not_included = [];
+      try {
+        const data = fs.readFileSync(file, {
+          encoding: "utf-8",
+        });
+        const thisLang = JSON.parse(data);
+        const thisLangKeys = Object.keys(thisLang);
+        not_included = targetKeys.filter(
+          (t_key) => !thisLangKeys.includes(t_key)
+        );
+      } catch (e) {
+        console.error(`Unexpected Error: ${e}`);
+      }
+      if (not_included.length > 0) {
+        throw new Error(`Missing keys for locale ${file}: \n - ${not_included.join(" \n - ")}\n\n`);
+      }
+    }
+  });
+};
 
-main()
+main();
