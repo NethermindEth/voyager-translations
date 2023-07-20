3b769efa6b166ec66c0d955527473a0410d678ab
Prince Anuragi <princanurag07@gmail.com>
2023-07-20 14:06:25 +0530

    imporved script to take pairs as args

--- add-key.js
+++ add-key.js
@@ -33,42 +33,52 @@ function getFiles(dir, files = []) {
 function add_key() {
     const args = process.argv.slice(2);
 
-    const [new_key, new_value] = args;
-    console.log("Adding Pair: ", new_key, ":", new_value)
-
-    // We already know this
-    const enPath = "./en/translation.json";
-    const enData = fs.readFileSync(enPath, {
-        encoding: "utf-8",
-    });
-    const targetKeys = Object.keys(JSON.parse(enData));
-
-    if (targetKeys.includes(new_key)) {
-        throw new Error(`Key Already Exists. Check ${enPath}. Aborting`);
+    if (args.length % 2 !== 0) {
+        throw new Error("Even number or args required. Key+Value like")
     }
+
+    const pairs = [];
+    for (let i = 0; i < args.length; i += 2) {
+        if (i + 1 < args.length) {
+            pairs.push([args[i], args[i + 1]]);
+        } else {
+            pairs.push([args[i]]);
+        }
+    }
+
+    console.log("Command-line argument pairs:");
+    console.log(pairs);
 
     const files = getFiles(".");
 
-    files.forEach((file) => {
-        console.info(`Wiriting locale ${file} | Adding ${new_key} ":" ${new_value}`)
-        try {
-            const data = fs.readFileSync(file, {
-                encoding: "utf-8",
-            });
-            const thisLang = JSON.parse(data);
-            const thisLangKeys = Object.keys(thisLang);
-            if (!thisLangKeys.includes(new_key)) {
-                // If this lang doesn't contain this key then only update
-                const newFileData = {
-                    ...thisLang,
-                    [new_key]: new_value
-                };
-                fs.writeFileSync(file, JSON.stringify(newFileData, null, 2))
+    pairs.forEach((pair) => {
+        const [new_key, new_value] = pair;
+
+        files.forEach((file) => {
+            try {
+                const data = fs.readFileSync(file, {
+                    encoding: "utf-8",
+                });
+                const thisLang = JSON.parse(data);
+                const thisLangKeys = Object.keys(thisLang);
+                if (!thisLangKeys.includes(new_key)) {
+                    console.info(`Wiriting locale ${file} | Adding ${new_key} ":" ${new_value}`)
+                    // If this lang doesn't contain this key then only update
+                    const newFileData = {
+                        ...thisLang,
+                        [new_key]: new_value
+                    };
+                    fs.writeFileSync(file, JSON.stringify(newFileData, null, 2))
+                } else {
+                    console.warn(`locale ${file} already has ${new_key}. Skipping`)
+                }
+            } catch (e) {
+                console.error(`Unexpected Error: ${e}`);
             }
-        } catch (e) {
-            console.error(`Unexpected Error: ${e}`);
-        }
-    });
+        });
+    })
+
+
 
 }
 
