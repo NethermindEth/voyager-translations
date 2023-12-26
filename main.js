3b9b467a2a197c84dfa4ff3d92ba3d2341386409
Haider <habbas33@gmail.com>
2023-12-26 15:18:12 +0500

    add translations (#64)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs")
 
 const ignoreFiles = [
   ".github",
@@ -8,59 +8,57 @@ const ignoreFiles = [
   "main.js",
   "add-key.js",
   "README.md",
-];
+]
 
 function getFiles(dir, files = []) {
   // Get an array of all files and directories in the passed directory using fs.readdirSync
-  const fileList = fs.readdirSync(dir);
+  const fileList = fs.readdirSync(dir)
 
-  const filteredFileList = fileList.filter(
-    (file) => !ignoreFiles.includes(file)
-  );
+  const filteredFileList = fileList.filter(file => !ignoreFiles.includes(file))
 
   for (const file of filteredFileList) {
-    const name = `${dir}/${file}`;
+    const name = `${dir}/${file}`
 
     if (fs.statSync(name).isDirectory()) {
-      getFiles(name, files);
+      getFiles(name, files)
     } else {
-      files.push(name);
+      files.push(name)
     }
   }
-  return files;
+  return files
 }
 
 const main = () => {
   // We already know this
-  const enPath = "./en/translation.json";
+  const enPath = "./en/translation.json"
   const enData = fs.readFileSync(enPath, {
     encoding: "utf-8",
-  });
-  const targetKeys = Object.keys(JSON.parse(enData));
+  })
+  const targetKeys = Object.keys(JSON.parse(enData))
 
-  const files = getFiles(".");
+  const files = getFiles(".")
 
-  files.forEach((file) => {
+  files.forEach(file => {
     if (file != enPath) {
-        console.info(`Checking locale ${file}`)
-      let not_included = [];
+      console.info(`Checking locale ${file}`)
+      let not_included = []
       try {
         const data = fs.readFileSync(file, {
           encoding: "utf-8",
-        });
-        const thisLang = JSON.parse(data);
-        const thisLangKeys = Object.keys(thisLang);
-        not_included = targetKeys.filter(
-          (t_key) => !thisLangKeys.includes(t_key)
-        );
+        })
+        const thisLang = JSON.parse(data)
+        const thisLangKeys = Object.keys(thisLang)
+        not_included = targetKeys.filter(t_key => !thisLangKeys.includes(t_key))
       } catch (e) {
-        console.error(`Unexpected Error: ${e}`);
+        console.error(`Unexpected Error: ${e}`)
       }
       if (not_included.length > 0) {
-        throw new Error(`Missing keys for locale ${file}: \n - ${not_included.join(" \n - ")}\n\n`);
+        throw new Error(
+          `Missing keys for locale ${file}: \n - ${not_included.join(" \n - ")}\n\n`
+        )
       }
     }
-  });
-};
+  })
+}
 
-main();
+main()
